8fc6efcffb259406e0d526b0f4bca108053f4512
Twan H <huiskenstwan@gmail.com>
2021-04-02 13:31:36 +0200

    add settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -6,26 +6,27 @@ vim.o.encoding     = 'utf-8'
 vim.o.fileencoding = 'utf-8'
 vim.o.guifont      = 'Fira\\ Code' -- Use fira code font
 
--- Tabs vs Spaces xD
-vim.o.shiftwidth  = 2 -- Use 2 spaces for auto indent by default
+-- Tabs 
+vim.o.shiftwidth  = 2    -- Use 2 spaces for auto indent by default
 vim.o.expandtab   = true -- Convert tab to spaces
 vim.o.smartindent = true -- Better indentation
 
 -- Saving and file management
 vim.o.backup   = false -- Do not use backup files
 vim.o.swapfile = false -- Do not use swapfiles
-vim.o.hidden   = true -- Keep multiple buffers open
+vim.o.hidden   = true  -- Keep multiple buffers open
 
 -- Visual stuff 
 -- Use relativenumbers but show the current line number
 vim.o.number         = true
-vim.o.relativenumber = true 
-vim.o.wrap           = false -- DO NOT USE WRAP
+vim.o.relativenumber = true
+vim.o.wrap           = false  -- DO NOT USE WRAP
 vim.o.background     = 'dark'
-vim.wo.signcolumn    = 'yes' -- Always show the sign columnm, otherwise it creates random movement
-vim.o.showtabline    = 2 -- Show tabs
+vim.wo.signcolumn    = 'yes'  -- Always show the sign columnm, otherwise it creates random movement
+vim.o.showtabline    = 2      -- Show tabs
+vim.o.cursorline     = true
 vim.o.termguicolors  = true
--- Comletion
+ -- Completion
 vim.o.completeopt = "menuone,noselect"
 
 -- Spelling
@@ -34,12 +35,18 @@ vim.o.spelllang = 'en,nl' -- Make sure that nl lang is installed in spell dir
 vim.cmd('autocmd FileType markdown setlocal spell')
 vim.cmd('autocmd FileType gitcommit setlocal spell')
 
+-- Searching
+vim.o.ignorecase = true
+
+-- Window management
+vim.o.splitright = true  -- Split to the right
+vim.o.splitbelow = false -- Make sure to split above the curren window
 
 -- AUTO COMMANDS
 -- Creating autocommand with vim.cmd does not always work correctly
 -- nvim_utitls has a good helper function for this.
 
--- Alll autocmd should be stored in this table
+-- All autocmd should be stored in this table
 local autocmds = {
   -- Highlight when yanking
   highlight_yank = {
