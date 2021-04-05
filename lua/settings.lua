c4f150595391fbb1134c67387c17d80b5b9d2223
Twan H <huiskenstwan@gmail.com>
2021-04-05 12:06:21 +0200

    Use the correct scope for options
    Set all the options in their correct scope using set_option
    
    Options need to be set in the correct scope
    (e.g. buffer, window or global) in order to work.
    `:set` in vim handles this automatically but in lua this
    needs to be done manually.
    
    To help with this set_options sets the option in the given scope and
    global scope as well.
    
    To find out which scope an option should be set in see `:help <option>`

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,50 +1,66 @@
 -- General settinsg
 -- Used to be lets and sets...
 
+-- Setting options using vim.o is not enough
+-- Some options need to be set in window scope (vim.wo), or buffer scope (vim.bo)
+-- To find which options needs to be set in what scope see: `:help <option>`
+-- In order to make it more easy to set all these options correctly I use set_option
+-- Until pull request #13479 is merged this is a okay alternative
+
+-- The scopes options can be assigned to
+local option_scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+-- Set the option in the correct scope
+local function set_option(scope, key, value)
+  option_scopes[scope][key] = value
+  -- If the options is in window or buffer scope set it in global scope aswell
+  if scope ~= 'o' then option_scopes['o'][key] = value end
+end
+
 -- File encoding 
-vim.o.encoding     = 'utf-8'
-vim.o.fileencoding = 'utf-8'
-vim.o.guifont      = 'Fira\\ Code' -- Use fira code font
+set_option('o', 'encoding', 'utf-8')
+set_option('o', 'fileencoding', 'utf-8')
+set_option('o', 'guifont', 'Fira\\ Code') -- Use fira code nerd font
 
 -- Tabs 
-vim.o.shiftwidth  = 2    -- Use 2 spaces for auto indent by default
-vim.o.expandtab   = true -- Convert tab to spaces
-vim.o.smartindent = true -- Better indentation
+set_option('b', 'shiftwidth', 2)      -- Use 2 spaces for auto indent by default
+set_option('b', 'expandtab', true)    -- Convert tab to spaces
+set_option('b', 'smartindent', true)  -- Better indentation
 
 -- Saving and file management
-vim.o.backup   = false -- Do not use backup files
-vim.o.swapfile = false -- Do not use swapfiles
-vim.o.hidden   = true  -- Keep multiple buffers open
+set_option('o', 'backup', false)   -- Do not use backup files
+set_option('o', 'swapfile', false) -- Do not use swapfiles
+set_option('o', 'hidden', true)    -- Keep multiple buffers open
 
 -- Visual stuff 
 -- Use relativenumbers but show the current line number
-vim.o.number         = true
-vim.o.relativenumber = true
-vim.o.wrap           = false  -- DO NOT USE WRAP
-vim.o.background     = 'dark'
-vim.wo.signcolumn    = 'yes'  -- Always show the sign columnm, otherwise it creates random movement
-vim.o.showtabline    = 2      -- Show tabs
-vim.o.cursorline     = true
-vim.o.termguicolors  = true
+set_option('w', 'number', true)
+set_option('w', 'relativenumber', true)
+set_option('w', 'wrap', false)          -- DO NOT USE WRAP
+set_option('o', 'background', 'dark')
+set_option('w', 'signcolumn', 'yes')    -- Always show the sign columnm, otherwise it creates random movement
+set_option('o', 'showtabline', 2)       -- Show tabs
+set_option('w', 'cursorline', true)
+set_option('o', 'termguicolors', true)
  -- Completion
-vim.o.completeopt = "menuone,noselect"
+set_option('o', 'completeopt',  'menuone,noselect')
 
 -- Spelling
-vim.o.spelllang = 'en,nl' -- Make sure that nl lang is installed in spell dir
+set_option('b', 'spelllang', 'en,nl') -- Make sure that nl lang is installed in spell dir
 -- Auto enable spell checking in markdown file and git commit messages
 vim.cmd('autocmd FileType markdown setlocal spell')
 vim.cmd('autocmd FileType gitcommit setlocal spell')
 
 -- Searching
-vim.o.ignorecase = true
+set_option('o', 'ignorecase', true)
 
 -- Window management
-vim.o.splitright = true  -- Split to the right
-vim.o.splitbelow = false -- Make sure to split above the curren window
+set_option('o', 'splitright', true)  -- Split to the right
+set_option('o', 'splitbelow', false) -- Make sure to split above the curren window
 
 -- AUTO COMMANDS
 -- Creating autocommand with vim.cmd does not always work correctly
--- nvim_utitls has a good helper function for this.
+-- nvim_utils has a good helper function for this.
 
 -- All autocmd should be stored in this table
 local autocmds = {
@@ -55,4 +71,3 @@ local autocmds = {
 }
 -- Create the autogroups with help from nvim_utitls
 nvim_create_augroups(autocmds)
-
