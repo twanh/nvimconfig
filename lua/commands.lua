876c97720d97056c96c2331cf72d7ac99d44c358
Twan H <huiskenstwan@gmail.com>
2021-05-22 17:45:08 +0200

    add command to edit todo file

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,3 +2,6 @@
 
 -- Create a commadn ReloadInit to reload the init.lua file
 vim.cmd('command! ReloadInit luafile ~/.config/nvim/init.lua')
+
+-- Edit todo
+vim.cmd('command!  Etodo vs $NOTESDIR/todo.md')
