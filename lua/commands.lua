0c2bf1f0e5141b011098b577b087e0ca1ce2a68c
Twan H <huiskenstwan@gmail.com>
2021-05-31 11:46:27 +0200

    add rename command

--- lua/commands.lua
+++ lua/commands.lua
@@ -5,4 +5,7 @@ vim.cmd('command! ReloadInit luafile ~/.config/nvim/init.lua')
 
 -- Edit todo file
 -- This command requires $NOTESDIR to be set
-vim.cmd('command!  Etodo vs $NOTESDIR/todo.md')
+vim.cmd('command! Etodo vs $NOTESDIR/todo.md')
+
+-- Rename (using) lsp command
+vim.cmd('command! Rename lua vim.lsp.buf.rename()')
