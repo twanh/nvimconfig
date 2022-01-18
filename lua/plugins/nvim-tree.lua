f85102334bdf373283748f47a707cbf0a8ecc72a
Twan H <huiskenstwan@gmail.com>
2022-01-18 21:29:24 +0100

    Update the nvim-tree plugin settings
    - Move some settings to the `setup` function
    - nvim-tree now has a function to auto find the current file in the
      tree, so using that instead of custom toggle function.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,9 @@
 -- Manage the settings for nvim-tree
 -- Ref: https://github.com/kyazdani42/nvim-tree.lua
--- Ignore common directories that have no value for most projects
-vim.g.nvim_tree_ignore = {'.git', '__pycache__'}
+
+-- Settings
 -- Show indent marker
 vim.g.nvim_tree_indent_markers = 1
--- Hide dotfiles files (dotfiles can always be show using `H` in the file explorer)
-vim.g.nvim_tree_hide_dotfiles = 1
 -- Don't show the git icons
 vim.g.nvim_tree_show_icons = {
   ['folders'] = 1,
@@ -13,31 +11,24 @@ vim.g.nvim_tree_show_icons = {
   ['git'] = 0,
 }
 
--- NvimTree keybinds
 
-if (vim.g.nvim_tree_auto_open == 1) then
-  vim.g.nvim_tree_find_file_open = true
-else
-  vim.g.nvim_tree_find_file_open = false 
-end
+-- Keybindings
+vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent = true })
 
-local function toggle_nvim_tree_find_file()
-  if (vim.g.nvim_tree_find_file_open) then
-    vim.g.nvim_tree_find_file_open = false 
-    vim.cmd('NvimTreeClose') 
-  else
-    vim.g.nvim_tree_find_file_open = true
-    vim.cmd('NvimTreeFindFile')
-  end
-end
-
--- Use the toggle function to toggle the find file functionallity 
-vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>lua require("plugins.nvim-tree").toggle_nvim_tree_find_file()<cr>', { noremap = true, silent = true })
-
+-- Settings
+-- Note that the plugin is slowly migrating all settings to the setup function
+-- until then some settings are set above
 require'nvim-tree'.setup {
   auto_open = true,
+  update_focused_file = {
+    enable      = true,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+
+  filters = {
+    dotfiles = true,
+    custom   = {'.git', '__pycache__'},
+  },
 }
 
-return {
-  toggle_nvim_tree_find_file = toggle_nvim_tree_find_file
-}
