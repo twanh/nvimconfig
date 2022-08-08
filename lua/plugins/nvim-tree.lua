b8e898d411796a0f8aa81f010f425589a9ffd145
Twan <twanh@pop-os.localdomain>
2022-08-08 14:42:17 +0200

    Update nvim-tree settings to use the new setup function

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,15 +2,6 @@
 -- Ref: https://github.com/kyazdani42/nvim-tree.lua
 
 -- Settings
--- Show indent marker
-vim.g.nvim_tree_indent_markers = 1
--- Don't show the git icons
-vim.g.nvim_tree_show_icons = {
-  ['folders'] = 1,
-  ['files'] = 1,
-  ['git'] = 0,
-}
-
 
 -- Keybindings
 vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>NvimTreeToggle<cr>', { noremap = true, silent = true })
@@ -19,13 +10,24 @@ vim.api.nvim_set_keymap('n', '<leader>b', '<cmd>NvimTreeToggle<cr>', { noremap =
 -- Note that the plugin is slowly migrating all settings to the setup function
 -- until then some settings are set above
 require'nvim-tree'.setup {
-  auto_open = true,
+  open_on_setup = true,
   update_focused_file = {
     enable      = true,
     update_cwd  = false,
     ignore_list = {}
   },
-
+  renderer = {
+    indent_markers = {
+      enable = false,
+    },
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        git = false,
+      },
+    },
+  },
   filters = {
     dotfiles = true,
     custom   = {'.git', '__pycache__'},
